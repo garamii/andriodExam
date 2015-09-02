9a96a2510d883d5ed3867fead6ba2985e4ca28f7
garam <flowsinyour@naver.com>
2015-09-02 11:11:19 +0900

    버튼 클릭시 visible,invisible 변경

---
+++ app/src/main/java/com/example/android/androidexam/layout/FrameLayoutActivity.java
@@ -0,0 +1,53 @@
+
+package com.example.android.androidexam.layout;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.example.android.androidexam.R;
+
+public class FrameLayoutActivity extends AppCompatActivity {
+
+    private Button mChangeBtn;
+    private ImageView mImageView1;
+    private ImageView mImageView2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frame_layout);
+
+        mChangeBtn = (Button) findViewById(R.id.change_btn);
+        mImageView1 = (ImageView) findViewById(R.id.image1);
+        mImageView2 = (ImageView) findViewById(R.id.image2);
+
+        mChangeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeImage();
+
+            }
+        });
+
+    }
+
+    public void changeImage() {
+
+        if (mImageView1.getVisibility() == View.VISIBLE) {
+            mImageView1.setVisibility(View.INVISIBLE);
+        } else if (mImageView1.getVisibility() != View.VISIBLE) {
+            mImageView1.setVisibility(View.VISIBLE);
+        }
+
+        if (mImageView2.getVisibility() == View.VISIBLE) {
+            mImageView2.setVisibility(View.INVISIBLE);
+        } else if (mImageView2.getVisibility() != View.VISIBLE) {
+            mImageView2.setVisibility(View.VISIBLE);
+        }
+
+    }
+
+}
