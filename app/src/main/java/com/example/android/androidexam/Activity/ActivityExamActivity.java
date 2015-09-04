35c650124dfaaa344d87e9c93483f75df34abe8e
garam <flowsinyour@naver.com>
2015-09-04 11:20:25 +0900

    startActivityResult, OnActivityResult, setResult 사용 예제

--- app/src/main/java/com/example/android/androidexam/Activity/ActivityExamActivity.java
+++ app/src/main/java/com/example/android/androidexam/Activity/ActivityExamActivity.java
@@ -1,3 +1,4 @@
+
 package com.example.android.androidexam.Activity;
 
 import android.content.Intent;
@@ -5,11 +6,13 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.android.androidexam.R;
 
 public class ActivityExamActivity extends AppCompatActivity implements View.OnClickListener {
 
+    public static final int REQUEST_CODE_SRTING = 1000;
     private EditText mPhoneEditText;
     private EditText mNameEditText;
 
@@ -19,20 +22,71 @@ public class ActivityExamActivity extends AppCompatActivity implements View.OnCl
         setContentView(R.layout.activity_activity_exam);
 
         findViewById(R.id.btn1).setOnClickListener(this);
+        findViewById(R.id.btn2).setOnClickListener(this);
 
-         mNameEditText = (EditText) findViewById(R.id.name_edit_text);
+        mNameEditText = (EditText) findViewById(R.id.name_edit_text);
         mPhoneEditText = (EditText) findViewById(R.id.phone_edit_text);
     }
 
     @Override
     public void onClick(View v) {
-        //targetActivity로 이동
-        Intent intent = new Intent(getApplicationContext(),TargetActivity.class);
+        switch (v.getId()) {
+            case R.id.btn1: {
+                btnclick();
+                break;
+            }
+            case R.id.btn2: {
+                btn2click();
+                // targetActivity로 이동
+            }
+            break;
+        }
+
+    }
+
+    private void btn2click() {
+        Intent intent = new Intent(getApplicationContext(), TargetActivity.class);
 
         // Data 추가
-        intent.putExtra("name",mNameEditText.getText().toString());
-        intent.putExtra("phone",mPhoneEditText.getText().toString());
+        intent.putExtra("name", mNameEditText.getText().toString());
+        intent.putExtra("phone", mPhoneEditText.getText().toString());
+
+        //================== 주는 곳 ===========================
+        //데이터를 돌려 받기 위해서는 startActivityForResult 로 호출해야됨
+        // 리퀘스트 코드는 원하는 값을 받기 위한 약속 같은것
+        startActivityForResult(intent, REQUEST_CODE_SRTING);
+    }
+
+    //================== 받는 곳 ===========================
+    //startActivityForResult 로 호출후에 데이터를 리턴 받는곳
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        //결과가 OK냐
+        if (resultCode == RESULT_OK) {
+            //requestCode 에 따라 처리를 나눈다
+            if (requestCode == REQUEST_CODE_SRTING) {
+                //data 가 왔으면
+                if (data != null) {
+                    String result = data.getStringExtra("result");
+                    Toast.makeText(ActivityExamActivity.this, "result:" + result, Toast.LENGTH_SHORT).show();
+                }
+            }
+        } else {
+            Toast.makeText(ActivityExamActivity.this, "에러", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void btnclick() {
+        Intent intent = new Intent(getApplicationContext(), TargetActivity.class);
+
+        // Data 추가
+        intent.putExtra("name", mNameEditText.getText().toString());
+        intent.putExtra("phone", mPhoneEditText.getText().toString());
 
         startActivity(intent);
     }
+
+
 }
--- app/src/main/java/com/example/android/androidexam/Activity/TargetActivity.java
+++ app/src/main/java/com/example/android/androidexam/Activity/TargetActivity.java
@@ -1,5 +1,6 @@
 package com.example.android.androidexam.Activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -9,6 +10,9 @@ import com.example.android.androidexam.R;
 
 public class TargetActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private String mName;
+    private String mPhone;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,16 +20,26 @@ public class TargetActivity extends AppCompatActivity implements View.OnClickLis
 
         //내가 호출 된 Intent로 부터 데이터 취득
 
-        String name = getIntent().getStringExtra("name");
-        String phone = getIntent().getStringExtra("phone");
+        mName = getIntent().getStringExtra("name");
+        mPhone = getIntent().getStringExtra("phone");
 
-        Toast.makeText(getApplicationContext(), "name :" + name + ",phone" + phone, Toast.LENGTH_SHORT).show();
+        Toast.makeText(getApplicationContext(), "name :" + mName + ",phone" + mPhone, Toast.LENGTH_SHORT).show();
 
         findViewById(R.id.finish_btn).setOnClickListener(this);
+
     }
 
     @Override
     public void onClick(View v) {
+        //돌려 줄 데이터를 Intent 에 설정
+        Intent intent = new Intent();
+        intent.putExtra("result",mName+","+mPhone);
+
+        // 결과와 데이터를 함께 돌려준다
+        setResult(RESULT_OK, intent);
+
+        //setResult(RESULT_CANCELED, intent);
+
         //Activity 종료
         finish();
     }
