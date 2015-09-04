37afe267ef3a60dd6f1f8592ec3db86ba7f3a64a
garam <flowsinyour@naver.com>
2015-09-04 12:02:22 +0900

    AlertDialog 예제

--- app/src/main/java/com/example/android/androidexam/Activity/ActivityExamActivity.java
+++ app/src/main/java/com/example/android/androidexam/Activity/ActivityExamActivity.java
@@ -1,8 +1,11 @@
 
 package com.example.android.androidexam.Activity;
 
+
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
@@ -23,6 +26,7 @@ public class ActivityExamActivity extends AppCompatActivity implements View.OnCl
 
         findViewById(R.id.btn1).setOnClickListener(this);
         findViewById(R.id.btn2).setOnClickListener(this);
+        findViewById(R.id.dialog_btn).setOnClickListener(this);
 
         mNameEditText = (EditText) findViewById(R.id.name_edit_text);
         mPhoneEditText = (EditText) findViewById(R.id.phone_edit_text);
@@ -31,16 +35,37 @@ public class ActivityExamActivity extends AppCompatActivity implements View.OnCl
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.btn1: {
+            case R.id.btn1:
+
                 btnclick();
                 break;
-            }
-            case R.id.btn2: {
+
+            case R.id.btn2:
                 btn2click();
+
                 // targetActivity로 이동
-            }
+            case R.id.dialog_btn:
+                openDialog();
+
             break;
         }
+
+    }
+
+    private void openDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(ActivityExamActivity.this);
+        builder.setTitle("타이틀");
+        builder.setMessage("메세지");
+        builder.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(ActivityExamActivity.this, "확인 눌렸어요", Toast.LENGTH_SHORT).show();
+            }
+        });
+        builder.setNegativeButton("닫기", null);
+        builder.setIcon(R.mipmap.ic_launcher);
+        builder.show();       // AlertDialog 를 최종 생성
 
     }
 
@@ -84,6 +109,7 @@ public class ActivityExamActivity extends AppCompatActivity implements View.OnCl
         // Data 추가
         intent.putExtra("name", mNameEditText.getText().toString());
         intent.putExtra("phone", mPhoneEditText.getText().toString());
+
 
         startActivity(intent);
     }
