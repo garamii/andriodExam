598567958dc964208f4da8d361830dce34eadf5c
garam <flowsinyour@naver.com>
2015-09-08 11:21:38 +0900

    달력 예제 -BaseAdapter 를 상속받은 calendarAdapter 의 기본 모양

---
+++ app/src/main/java/com/example/android/androidexam/calendar/CalendarAdapter.java
@@ -0,0 +1,45 @@
+package com.example.android.androidexam.calendar;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import java.util.Calendar;
+import java.util.List;
+
+/**
+ * Created by student on 2015-09-08.
+ * 달력 표시용 어뎁터
+ */
+public class CalendarAdapter extends BaseAdapter {
+
+    private List<Calendar> mList;
+    private Context mContext;
+
+    public CalendarAdapter(Context context , List<Calendar> list) {
+        mList = list;
+        mContext = context;
+    }
+
+    @Override
+    public int getCount() {
+
+        return mList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return mList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
