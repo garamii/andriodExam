ff9d5cd2a987ff1eb377700d97f1d78dfa9d7434
garam <flowsinyour@naver.com>
2015-09-24 11:45:14 +0900

    SharedPreference 예제

--- app/src/main/java/com/example/android/androidexam/database/LogInActivity.java
+++ app/src/main/java/com/example/android/androidexam/database/LogInActivity.java
@@ -1,11 +1,14 @@
 
 package com.example.android.androidexam.database;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -20,6 +23,7 @@ public class LogInActivity extends AppCompatActivity implements View.OnClickList
     private UserDbHelper mUserDbHelper;
     private EditText mEmail;
     private EditText mPassword;
+    private CheckBox mCheckBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,11 +33,24 @@ public class LogInActivity extends AppCompatActivity implements View.OnClickList
 
         mEmail = (EditText) findViewById(R.id.edit_email);
         mPassword = (EditText) findViewById(R.id.edit_password);
+        mCheckBox = (CheckBox) findViewById(R.id.check_email);
 
         findViewById(R.id.tv_sign_up).setOnClickListener(this);
         findViewById(R.id.btn_login).setOnClickListener(this);
 
         mUserDbHelper = new UserDbHelper(this);
+        //TODO  mEmail 에 SharedPreference 에 저장된값이 있으면 가져와서 셋팅
+
+        mEmail.setText(loadEmail());
+
+    }
+
+    private String loadEmail() {
+
+        SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
+        String email = sharedPref.getString("email" , "");
+        return email;
+
     }
 
     @Override
@@ -58,6 +75,13 @@ public class LogInActivity extends AppCompatActivity implements View.OnClickList
                         if (email.equals(mEmail.getText().toString())&&
                                 password.equals(mPassword.getText().toString())) {
                             Toast.makeText(LogInActivity.this, "로그인 성공", Toast.LENGTH_SHORT).show();
+                            //TODO SharedPreference 저장하는 부분
+
+                            if(mCheckBox.isChecked()){
+                                saveEmail(mEmail.getText().toString());
+                            }else {
+                                saveEmail("");
+                            }
                             return;
                         }
                     }
@@ -67,5 +91,13 @@ public class LogInActivity extends AppCompatActivity implements View.OnClickList
                 Toast.makeText(LogInActivity.this, "이메일 또는 패스워드가 틀렸습니다", Toast.LENGTH_SHORT).show();
                 break;
         }
+    }
+
+    private void saveEmail(String email) {
+        SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString("pref_email", email);
+        editor.commit();  //동기식 sync
+        //editor.apply(); //비동기식 async
     }
 }
